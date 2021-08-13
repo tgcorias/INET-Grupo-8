8f6cabb1496c6b98b86ab1b8f2e3a1f517dea5d7
Ticiano Corias <tcp@pop-os.localdomain>
2021-08-12 23:35:35 -0300

    Buenas prácticas

--- app.js
+++ app.js
@@ -29,31 +29,31 @@ app.use(session({
 }))
 
 //8- Invocamos al modulo de conexion de la base de datos
-const connection = require("./database/db")
+const connection = require("./database/db");
 
 //9- Estableciendo las rutas
 app.get("/", (req,res) => {
-    res.render("index")
+    res.render("index");
 })
 app.get("/login", (req,res) => {
-    res.render("login")
+    res.render("login");
 })
 app.get("/agregar", (req,res) => {
-    res.render("agregar")
+    res.render("agregar");
 })
 
 //10- Registro
 app.post("/agregar", async (req, res)=>{
-  const nombre_local = req.body.nombre_local
-  const nombre_responsable = req.body.nombre_responsable
-  const direccion = req.body.direccion
-  const ciudad = req.body.ciudad
-  const provincia = req.body.provincia
-  const email = req.body.email
-  const telefono = req.body.telefono
-  const capacidad_maxima = req.body.capacidad_maxima
-  const password = req.body.password
-  let passwordHash = await bcryptjs.hash(password, 8)
+  const nombre_local = req.body.nombre_local;
+  const nombre_responsable = req.body.nombre_responsable;
+  const direccion = req.body.direccion;
+  const ciudad = req.body.ciudad;
+  const provincia = req.body.provincia;
+  const email = req.body.email;
+  const telefono = req.body.telefono;
+  const capacidad_maxima = req.body.capacidad_maxima;
+  const password = req.body.password;
+  let passwordHash = await bcryptjs.hash(password, 8);
   connection.query("INSERT INTO locales_usuarios SET ?", {
     nombre_local:nombre_local,
     nombre_responsable:nombre_responsable,
@@ -69,7 +69,7 @@ app.post("/agregar", async (req, res)=>{
       console.log(error);
     }
     else {
-      res.render("agregar")
+      res.render("agregar");
     }
   })
 })
